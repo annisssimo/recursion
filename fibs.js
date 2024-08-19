3d83298a8118afd6e56cba6d11b559e555c6380c
annisssimo <anyalar04@gmail.com>
2024-08-19 11:16:55 +0300

    feat: solution to fibonacci with recursion

--- fibs.js
+++ fibs.js
@@ -12,4 +12,18 @@ const fibs = (num) => {
   return array;
 };
 
-console.log(fibs(8));
+console.log(fibs(4));
+
+const fibsRec = (n) => {
+  if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    const array = fibsRec(n - 1);
+    array.push(array[array.length - 1] + array[array.length - 2]);
+    return array;
+  }
+};
+
+console.log(fibsRec(4));
